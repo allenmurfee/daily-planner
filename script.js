3fb73e72642732008e99d1e29eaad46207769aed
allenmurfee <allenmurfee123@gmail.com>
2022-09-16 10:24:22 -0400

    local storage is working. still need to figure out time and coloring linking

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 var saveBtn = $("btn");
 var today = moment().format("MM.DD.YYYY");
 var hour = $(".hour");
-var currentHour = moment(hour).format("h")
-var now = moment();
+var currentHour = moment(hour).format("h");
 
+$("textarea").each(function () {
+  $(this).val(localStorage.getItem($(this).attr("id")));
+});
 
 //Sets day
 $("#currentDay").text("Today is " + today);
-
 
 //Changes color based on hour of day
 function time() {
   $(".timeblock").each(function () {
     var plannerHour = parseInt($(this).children(0).text());
     console.log(plannerHour);
-    console.log(currentHour)
+    console.log(currentHour);
     if (plannerHour < currentHour) {
-      $(this).addClass("past")
+      $(this).addClass("past");
     } else if (plannerHour == currentHour) {
-      $(this).addClass("present")
+      $(this).addClass("present");
     } else if (plannerHour > currentHour) {
       $(this).addClass("future");
     }
   });
 }
 
-
 //Calls time function
 time();
 
-
-// Saves text area input value
-$(".btn").on ("click", function () {
+$(".btn").on("click", function () {
   // Need to set local storage to be user text input
-    var textInput = $(this).siblings(".input").val();
+  $("textarea").each(function () {
+    var id = $(this).attr("id");
+    var textInput = $(this).val();
     console.log(textInput);
-    localStorage.setItem("input", textInput)
+    localStorage.setItem(id, textInput);
   });
+});
